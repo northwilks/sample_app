0f40f8b07180f20f7ca62700ba335c3381242971
Tom Wilkins <northwilks@gmail.com>
2014-07-09 15:44:20 -0400

    add a StatucPages controller

--- config/initializers/back_secret_token.rb
+++ config/initializers/back_secret_token.rb
@@ -9,21 +9,15 @@
 
 # Make sure your secret_key_base is kept private
 # if you're sharing your code publicly.
-# Be sure to restart your server when you modify this file.
-
-# Make sure your secret_key_base is kept private
-# if you're sharing your code publicly, such as by adding
-# .secret to your .gitignore file.
-
 require 'securerandom'
 
 def secure_token
   token_file = Rails.root.join('.secret')
   if File.exist?(token_file)
-    # Use the existing token.
-    File.read(token_file).chomp
+    # Use the existing token
+    File.read(token.file).chomp
   else
-    # Generate a new token and store it in token_file.
+    # Generate new token and store
     token = SecureRandom.hex(64)
     File.write(token_file, token)
     token
@@ -31,4 +25,3 @@ def secure_token
 end
 
 SampleApp::Application.config.secret_key_base = secure_token
-
